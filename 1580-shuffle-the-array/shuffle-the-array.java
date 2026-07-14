0fadf1bbd5dd5da0fda8201ee8e95bae406012ff
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2026-07-14 15:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (22.25%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,12 +1,17 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-         int arr[] = new int[n * 2];
-
-        for(int i=0; i<n; i++)
-        {
-            arr[i*2] = nums[i];
-            arr[i*2+1] = nums[i+n];
+        int[] arr = new int[2 * n];
+        int left = 0; // Pointer for x1, x2, ..., xn
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                arr[i] = nums[left]; 
+                left++; // Move to the next x element
+            } else {
+                // The corresponding y element is always exactly 'n' positions ahead of its x partner
+                arr[i] = nums[left - 1 + n]; 
+            }
         }
-        return arr;
+        return arr;  
     }
 }
