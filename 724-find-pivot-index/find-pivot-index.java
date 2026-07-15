a37cdc575137b84f6ed9764a9112490b3dda0277
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2026-07-15 12:11:57 +0530

    Time: 1 ms (98.72%) | Memory: 47.2 MB (88.97%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,27 +1,52 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
-        
 
-        for(int i=0; i<nums.length; i++)
-        {
-            int lsum = 0;
-            int rsum = 0;
+    public int pivotIndex(int[] arr) {
 
-            for(int j=0; j<i; j++)
-            {
-                lsum = lsum + nums[j];
-            }
+        // Edge case: Only one element
+        if (arr.length == 1) {
+            return 0;
+        }
 
-            for(int k=i+1; k<nums.length; k++)
-            {
-                rsum = rsum+nums[k];
-            }
+        int[] pf = prefixSum(arr);
 
-            if(lsum==rsum)
-            {
+        // Check index 0
+        if (pf[arr.length - 1] - pf[0] == 0) {
+            return 0;
+        }
+
+        // Check middle indices
+        for (int i = 1; i < arr.length - 1; i++) {
+
+            // Sum of elements before index i
+            int leftSum = pf[i - 1];
+
+            // Sum of elements after index i
+            int rightSum = pf[arr.length - 1] - pf[i];
+
+            if (leftSum == rightSum) {
                 return i;
             }
         }
+
+        // Check last index
+        if (pf[arr.length - 2] == 0) {
+            return arr.length - 1;
+        }
+
+        // No pivot index found
         return -1;
+    }
+
+    static int[] prefixSum(int[] arr) {
+
+        int[] pf = new int[arr.length];
+
+        pf[0] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            pf[i] = pf[i - 1] + arr[i];
+        }
+
+        return pf;
     }
 }
