0b69b47987cef439295153114798ed16f12679e1
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2026-07-28 07:31:54 +0530

    Time: 33 ms (43.78%) | Memory: 48.3 MB (23.69%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        StringBuilder firstHalf = new StringBuilder();
+        char middleChar = 0;
+
+        // Build the lexicographically smallest left half
+        for (int i = 0; i < 26; i++) {
+            if (count[i] % 2 != 0) {
+                middleChar = (char) ('a' + i);
+            }
+            for (int j = 0; j < count[i] / 2; j++) {
+                firstHalf.append((char) ('a' + i));
+            }
+        }
+
+        // Construct the full palindrome
+        StringBuilder result = new StringBuilder(firstHalf);
+        if (middleChar != 0) {
+            result.append(middleChar);
+        }
+        result.append(new StringBuilder(firstHalf).reverse());
+
+        return result.toString();
+    }
+}
