34a02353e4a1e2b90021317a5e998638a5556c0b
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2026-07-31 12:10:46 +0530

    Time: 2101 ms (30.23%) | Memory: 41.8 MB (90.99%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        
+      //  boolean isPrefect = false;
+        int sum = 0;
+        int n = num;
+        for(int i=1; i<num; i++)
+        {
+            if(num%i==0)
+            {
+                sum = sum+i;
+            }
+        }
+        if(sum==n)
+        {
+            return true;
+        }
+        return false;
+    }
+}
